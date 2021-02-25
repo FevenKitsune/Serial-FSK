2aaba0871228769fe4ff565a918b0db0bc4a8f47
Feven Kitsune <fevenkitsune@gmail.com>
2021-02-25 10:49:29 -0500

    + Initial implementation, will merge features from LoRa serial ASAP.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <RadioLib.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// LoRa Module pinouts based on https://learn.adafruit.com/assets/46254
+RFM95 fsk = new Module(8, 3, 4);
+
+void setup()
+{
+  Serial.begin(9600);
+
+  int state = fsk.beginFSK();
+  Serial.print(F("fsk.beginFSK() returned "));
+  Serial.println(state);
+
+  if (state == ERR_NONE)
+  {
+    Serial.println(F("fsk.beginFSK() success!"));
+  }
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
