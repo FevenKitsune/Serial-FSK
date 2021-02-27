148a063270136319cde8d44b309e84c22d2194ba
Feven Kitsune <fevenkitsune@gmail.com>
2021-02-27 15:33:29 -0500

    + startup improvement

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <RadioLib.h>
 #include "run.h"
 
+#define STATUS_PIN 13
+
 // LoRa Module pinouts based on https://learn.adafruit.com/assets/46254
 RFM95 fsk = new Module(8, 3, 4);
 
@@ -10,15 +12,29 @@ String serial_data;
 
 void setup()
 {
+  pinMode(STATUS_PIN, OUTPUT);
+
   Serial.begin(9600);
+  Serial.setTimeout(50);
+
+  while(!Serial)
+  {
+    // Enable light while waiting for Serial.
+    digitalWrite(STATUS_PIN, HIGH);
+  }
+  // Disable light when Serial is connected.
+  digitalWrite(STATUS_PIN, LOW);
+
+  Serial.println(F("[INIT] Serial connected!"));
+  Serial.println(F("[INIT] Starting RFM95 in FSK mode..."));
 
   int state = fsk.beginFSK();
-  Serial.print(F("fsk.beginFSK() returned "));
+  Serial.print(F("[INIT] fsk.beginFSK() returned "));
   Serial.println(state);
 
   if (state == ERR_NONE)
   {
-    Serial.println(F("fsk.beginFSK() success!"));
+    Serial.println(F("[INIT] fsk.beginFSK() success!"));
   }
 }
 
