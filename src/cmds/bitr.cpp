4f92d06e179babc1a1ed85b9b413d806309e4ab6
Feven Kitsune <fevenkitsune@gmail.com>
2021-03-05 16:11:09 -0500

    - removed integrated value check - missing newline

--- src/cmds/bitr.cpp
+++ src/cmds/bitr.cpp
@@ -2,12 +2,8 @@
 
 int16_t bitr(float bitrate, RFM95 &radio)
 {
-    if ((bitrate < BITR_MIN) || (bitrate > BITR_MAX))
-    {
-        return INVALID_PARAMETER;
-    }
     Serial.print(F("[BITR] Bitrate set to "));
     Serial.print(bitrate);
-    Serial.print(F(" kbps"));
+    Serial.println(F(" kbps"));
     return radio.setBitRate(bitrate);
 }
