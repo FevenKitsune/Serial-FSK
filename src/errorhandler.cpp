4cc0039fd3adb9067bbcb133cc3c02cb236500d2
Feven Kitsune <fevenkitsune@gmail.com>
2021-03-05 13:10:53 -0500

    + improved documentation for error handler.

--- src/errorhandler.cpp
+++ src/errorhandler.cpp
@@ -2,11 +2,16 @@
 
 int16_t printError(int16_t errorCode)
 {
+    // Pretty-print prefix for error logging.
     Serial.print(F(ERROR_PREFIX));
+    
+    // Error handling switch statement.
     switch (errorCode)
     {
-    case ERR_NONE:
-        break;
+    /*
+     * System error handling.
+     * All error codes implemented by the developer are handled and printed here.
+     */
     case COMMAND_NOT_FOUND:
         Serial.println(F("Unknown command."));
         break;
@@ -16,15 +21,32 @@ int16_t printError(int16_t errorCode)
     case NOT_YET_IMPLEMENTED:
         Serial.println(F("This command is not implemented yet."));
         break;
+
+    /*
+     * RadioLib error handling.
+     * All error codes implemented by RadioLib are handled and printed here.
+     */
+    case ERR_NONE:
+        break;
     case ERR_INVALID_FREQUENCY_DEVIATION:
         Serial.println(F("Invalid frequency deviation. Allowed values are dependant on bitrate."));
         break;
     case ERR_INVALID_BIT_RATE:
         Serial.println(F("Invalid bitrate."));
+
+    /*
+     * Unknown error handling.
+     * The error has not been handled. Print error code.
+     */
     default:
         Serial.print(F("An unknown error has occured. Code "));
         Serial.println(errorCode);
         break;
     }
+
+    /* 
+     * Do not return anynthing except ERR_NONE in error handler.
+     * If your error handler is throwing an error, God help you.
+     */
     return ERR_NONE;
 }
