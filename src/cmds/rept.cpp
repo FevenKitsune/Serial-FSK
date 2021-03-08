ec2e6e15eb6e7a2d5420278fe1b77d467d4a2585
Feven Kitsune <fevenkitsune@gmail.com>
2021-03-08 18:32:56 -0500

    + Range alignment

--- src/cmds/rept.cpp
+++ src/cmds/rept.cpp
@@ -6,7 +6,7 @@ int16_t rept(uint8_t interval, uint16_t millisDelay, String data, RFM95 &radio)
     for (uint8_t i = 0; i < interval; i++)
     {
         Serial.print(F("[REPT] ["));
-        Serial.print(i);
+        Serial.print(i+1);
         Serial.print(F("/"));
         Serial.print(interval);
         Serial.print(F("] "));
